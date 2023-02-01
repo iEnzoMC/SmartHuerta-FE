d10ff7ca71b939e007f7110ef728048d1da83ef3
Agustín Avellaneda <100479647+nitdraig@users.noreply.github.com>
2023-02-01 18:07:21 -0300

    IconLogoActualización-2

--- src/components/Login/Images/LoginLogo.icon.jsx
+++ src/components/Login/Images/LoginLogo.icon.jsx
@@ -4,7 +4,8 @@ import React from 'react'
 export default function LoginLogoicon  ()  {
   const style = {
       height:"100%",
-      width: "100%"
+      width: "100%",
+      paddingTop:" 35px"
     }
 
   return (
